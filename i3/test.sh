87ca915d820a6ad5acbe8c165e75d732bf00fb57
69Joebama69 <blablablacoc04@gmail.com>
2022-06-12 13:05:11 +0200

    update

--- i3/test.sh
+++
@@ -1,7 +0,0 @@
-get_options() {
-  echo "  Capture Region  Clip"
-  echo "  Capture Region  File"
-  echo "  Record Region  File (MP4)"
-}
-
-(get_options) | rofi -dmenu -i -fuzzy -p "Screenshot" -theme $HOME/.config/rofi/launchers/colorful/style_7.rasi
